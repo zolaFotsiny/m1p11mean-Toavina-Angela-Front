51bdb86552049348a08f88b101ecf501bb09883d
ToavinaMario <157911400+toavinaDev@users.noreply.github.com>
2024-02-07 21:06:26 +0300

    [refact] structure dossier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
-import { BannerComponent } from './banner/banner.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 
 @Component({
