ea3ab5bf529028160e38a4a8cc29a86a491ff153
ToavinaMario <157911400+toavinaDev@users.noreply.github.com>
2024-02-17 09:45:27 +0300

    Revert "dynamic form tsy mety"
    This reverts commit b43aafbb832fcfa48362524794ad27fc88a2af9a.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { ClientAddComponent } from './components/client-add/client-add.component';
+import { ServiceAddComponent } from './pages/admin/service/service-add/service-add.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { PersonnelListComponent } from './pages/admin/personnel/personnel-list/personnel-list.component';
-import { ServiceAddComponent } from './pages/admin/service/service-add/service-add.component';
 import { ServiceListeComponent } from './pages/admin/service/service-liste/service-liste.component';
 import { ClientComponent } from './pages/client/client.component';
 import { HomeComponent } from './pages/home/home.component';
