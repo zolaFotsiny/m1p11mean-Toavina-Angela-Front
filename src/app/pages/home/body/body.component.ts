242991c8662cd89559b7015610d99ee9589fa1a9
ToavinaMario <157911400+toavinaDev@users.noreply.github.com>
2024-02-19 22:29:25 +0300

    feat afficher plus service

--- src/app/pages/home/body/body.component.ts
+++ src/app/pages/home/body/body.component.ts
@@ -7,7 +7,7 @@ import { NzSpinModule } from 'ng-zorro-antd/spin';
 @Component({
   selector: 'home-body',
   standalone: true,
-  imports: [CommonModule, NzFlexModule, HomeComponent, NzSpinModule],
+  imports: [CommonModule, NzFlexModule, NzSpinModule],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss'
 })
@@ -15,24 +15,35 @@ export class BodyComponent implements OnInit {
   title = 'm1p11mean-Toavina-Angela-Front';
 
   services: any[] = [];
+  displayedServices: any[] = []; // Les services à afficher
+  itemsToShow = 4; // Nombre initial d'éléments à afficher
+
   employee: any[] = [];
   isLoading = true; // Add a loading flag
+
   constructor(private servicesService: ServicesService) { }
 
+  showMoreServices() {
+    // Afficher plus d'éléments à chaque clic sur le bouton
+    this.itemsToShow += 4; // Ajoute 4 éléments supplémentaires
+    this.displayedServices = this.services.slice(0, this.itemsToShow);
+  }
   ngOnInit(): void {
     console.log('ngOnInit called');
+    this.displayedServices = this.services.slice(0, this.itemsToShow);
     this.servicesService.getServices().subscribe(
       (response: any) => {
         console.log('Raw response:', response);
+
 
         // Access the 'data' property if it exists
         if (response && response.data) {
           this.services = response.data;
           console.log('Services:', this.services);
+          this.displayedServices = this.services.slice(0, this.itemsToShow);
         } else {
           console.error('Invalid response structure:', response);
         }
-
         // Set loading flag to false once data is obtained
         this.isLoading = false;
       },
