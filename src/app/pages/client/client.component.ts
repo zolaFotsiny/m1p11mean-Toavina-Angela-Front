860aa66414057366a8ca04b2bf8ee7fbb12c91cb
ToavinaMario <157911400+toavinaDev@users.noreply.github.com>
2024-02-21 22:02:09 +0300

    add rendez vous modal

--- src/app/pages/client/client.component.ts
+++ src/app/pages/client/client.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../home/header/header.component';
 import { JwtPayload, jwtDecode } from 'jwt-decode';
 import { CalendarRendezVousComponent } from './calendar-rendez-vous/calendar-rendez-vous.component';
+import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
+import { RendezvousComponent } from './rendezvous/rendezvous.component';
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [HeaderComponent, CalendarRendezVousComponent],
+  imports: [HeaderComponent, CalendarRendezVousComponent, CommonModule, RendezvousComponent],
   templateUrl: './client.component.html',
   styleUrl: './client.component.scss'
 })
 export class ClientComponent implements OnInit {
+  @ViewChild('content') content: any; // Declare the template reference variable
+  modalContent: any; // Property to store the modal content
+
   email: string = '';
   nom: string = '';
   id: string = '';
   prenom: string = '';
-
+  constructor(private modalService: NgbModal) { }
 
 
   ngOnInit(): void {
@@ -25,5 +31,35 @@ export class ClientComponent implements OnInit {
     this.nom = infoUser.nom;
     this.prenom = infoUser.prenom;
     this.id = infoUser.id;
+  }
+
+
+
+
+
+
+
+
+
+
+
+  openModal(content: any) {
+    const modalOptions: NgbModalOptions = {
+      centered: true,
+      size: 'lg'
+    };
+
+    this.modalContent = content; // Set the content for the modal
+
+    const modalRef = this.modalService.open(this.content, modalOptions);
+
+    modalRef.result.then(
+      (result) => {
+        // Handle modal close
+      },
+      (reason) => {
+        // Handle modal dismiss
+      }
+    );
   }
 }
