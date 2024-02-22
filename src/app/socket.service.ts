3fa1fd80c8fc2a89035f55d1bd5bd2c8a8d5c8ff
ToavinaMario <157911400+toavinaDev@users.noreply.github.com>
2024-02-23 00:00:08 +0300

    service socket client

---
+++ src/app/socket.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { Observable, fromEvent } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SocketService {
+    private socket: Socket;
+
+    constructor() {
+        this.socket = io('http://localhost:8080', { withCredentials: true });
+    }
+
+    connect(): void {
+        // Logic for connecting to Socket.IO if necessary
+    }
+
+
+    // listenForRdvEvent() {
+    //     return this.socket.fromEvent<any>('rdv');
+    // }
+    listenForRdvEvent(): Observable<any> {
+        return new Observable((observer) => {
+            this.socket.on('rdv', (data: any) => {
+                observer.next(data);
+            });
+        });
+    }
+
+    sendMessage(message: string): void {
+        this.socket.emit('message', message);
+    }
+
+    getMessage(): Observable<string> {
+        return fromEvent(this.socket, 'message') as Observable<string>;
+    }
+}
